e7060405a7553d793d75fad688be2ba5c4ff876a
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-18 14:36:04 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  small= { :books => 10, :magazines => 5}
+  name_hash(small)
+  hash.each do |name|
+  puts "#{key}
+end
 
 end
